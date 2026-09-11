acec9e2bebdd64345f407666319ce12ee80637c0
RENAN SILVA DE ABREU <26211008@dsn127m1247444.cfp127aluno.local>
2026-09-11 16:49:24 -0300

    Correcao

--- 03-menu.py
+++ 03-menu.py
@@ -42,10 +42,8 @@ def Somatorio():
     print(f"O somatório é:", {soma})
 def Fatorial():
     numero = int(input("Digite um número: "))
-
     contador = 1
     resultado = 1
-
     while contador <= numero:
         resultado = resultado * contador
         contador = contador + 1
